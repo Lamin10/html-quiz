af4ddadee6cdc2b53c163373873eb1a7f938e183
Lamin10 <>
2023-06-27 16:28:45 +0000

    Add questions and answers to the question file

---
+++ questions.js
@@ -0,0 +1,128 @@
+  questions = [{
+    "question": "What is HTML?",
+    "option1": "HighText Markup Language",
+    "option2": "HyperText Markup Language",
+    "option3": "hyperText Made Lines",
+    "option4": "HyperTax markup lines",
+    "answer": "1"
+},
+
+{
+    "question": "Which One is an HTML Extension?",
+    "option1": ".css",
+    "option2": ".js",
+    "option3": ".html",
+    "option4": ".hml",
+    "answer": "2"
+},
+
+{
+    "question": "Which HTML tag makes the text bold?",
+    "option1": "<b>",
+    "option2": "<a>",
+    "option3": "<bl>",
+    "option4": "<bd>",
+    "answer": "0"
+},
+
+{
+    "question": "Which HTML tag makes the text italic?",
+    "option1": "<i>",
+    "option2": "<italic>",
+    "option3": "<it>",
+    "option4": "<il>",
+    "answer": "0"
+},
+
+{
+    "question": "Which tag is used to define a line break",
+    "option1": "<brr>",
+    "option2": "<break>",
+    "option3": "<br>",
+    "option4": "<br>..</br>",
+    "answer": "2"
+},
+
+{
+    "question": "What does the <br> tag add to your webpage?",
+    "option1": "Paragraph break",
+    "option2": "Long break",
+    "option3": "Line break",
+    "option4": "None of the above",
+    "answer": "2"
+},
+
+{
+    "question": "Which of the following is a pragraph tag in HTML?",
+    "option1": "<paragraph>...</paragraph>",
+    "option2": "<p>...</p>",
+    "option3": "<ul>...</ul>",
+    "option4": "<pa>...</pa>",
+    "answer": "1"
+},
+{
+    "question": "All HTML tags are enclsed in?",
+    "option1": "{ and }",
+    "option2": "[ and ]",
+    "option3": "( and )",
+    "option4": "< and >",
+    "answer": "3"
+},
+{
+    "question": "The Body tag usually comes after the?",
+    "option1": "HTML tag",
+    "option2": "HEAD tag",
+    "option3": "TITLE tag",
+    "option4": "EM tag",
+    "answer": "1"
+},
+{
+    "question": "Which character represent the closing of a HTML tag?",
+    "option1": "/",
+    "option2": "+",
+    "option3": "%",
+    "option4": "!",
+    "answer": "0"
+},
+{
+    "question": "Which tag represent the start and end of a page?",
+    "option1": "<body></body>",
+    "option2": "<head></head>",
+    "option3": "<title></title>",
+    "option4": "<html></html>",
+    "answer": "3"
+},
+{
+    "question": "Which tag represent an unordered list?",
+    "option1": "<li></li>",
+    "option2": "<ul></ul>",
+    "option3": "<ol></ol>",
+    "option4": "<br>",
+    "answer": "1"
+},
+{
+    "question": "What does URL means?",
+    "option1": "United Research Locator",
+    "option2": "Uniform Research Locator",
+    "option3": "Uniform Resource Locator",
+    "option4": "United Refund Locator",
+    "answer": "2"
+},
+{
+    "question": "Which HTML tag produce the biggest heading?",
+    "option1": "<h1>",
+    "option2": "<h2>",
+    "option3": "<h4>",
+    "option4": "<h6>",
+    "answer": "0"
+},
+{
+    "question": "A narmal webpage consist of?",
+    "option1": "Body and Mind",
+    "option2": "Body and Legs",
+    "option3": "Head and Body",
+    "option4": "Top and Bottom",
+    "answer": "2"
+}
+    
+]
