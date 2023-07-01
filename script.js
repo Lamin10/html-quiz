09080a0dcb8581d218a62bc2497d76398dba6417
Lamin10 <>
2023-07-01 19:29:13 +0000

    Add media query to the quiz app

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let currentQuestion = 0;
 let score = 0;
 let totalQuestions = questions.length;
 
+/* Getting all the elements from the DOM */
+
 let container = document.getElementById("quizContainer");
 let questionEl = document.getElementById("question");
 let opt1 = document.getElementById("opt1");
@@ -10,7 +12,10 @@ let opt3 = document.getElementById("opt3");
 let opt4 = document.getElementById("opt4");
 let nextButton = document.getElementById("nextButton");
 let resultCont = document.getElementById("result");
+let scoreEl = document.getElementById("score");
 
+
+/* The loadQuestions function */
 
 function loadQuestion (questionIndex) {
     let q = questions[questionIndex];
@@ -21,6 +26,8 @@ function loadQuestion (questionIndex) {
     opt4.textContent = q.option4;
 };
 
+/* Load next question function */
+
 function loadNextQuestion () {
     let selectedOption = document.querySelector("input[type=radio]:checked");
     if (!selectedOption) {
@@ -30,7 +37,12 @@ function loadNextQuestion () {
     let answer = selectedOption.value;
     if (questions[currentQuestion].answer === answer) {
         score += 1;
+    } else {
+        score -= 1;
     }
+
+    scoreEl.innerHTML = `score: ${score}`;
+
     selectedOption.checked = false;
     currentQuestion++;
     if (currentQuestion == totalQuestions - 1) {
@@ -39,7 +51,7 @@ function loadNextQuestion () {
     if (currentQuestion == totalQuestions) {
         container.style.display = "none";
         resultCont.style.display = "";
-        resultCont.textContent = "Your Score"   +  score  + "/" + questions.length;
+        resultCont.textContent = "Your Score"   +  score   + "/" +  questions.length;
         return;
     }
     loadQuestion(currentQuestion);
