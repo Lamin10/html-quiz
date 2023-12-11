68748453907dfe424fee2f17378fc8039aee1be5
Lamin10 <>
2023-12-11 19:02:09 +0000

    Add eventlisteners to all buttons

--- script.js
+++ script.js
@@ -11,24 +11,24 @@ let opt2 = document.getElementById("opt2");
 let opt3 = document.getElementById("opt3");
 let opt4 = document.getElementById("opt4");
 let nextButton = document.getElementById("nextButton");
+let restartQuizButton = document.getElementById("restartButton");
 let resultCont = document.getElementById("result");
 let scoreEl = document.getElementById("score");
 
-
 /* The loadQuestions function */
 
-function loadQuestion (questionIndex) {
+function loadQuestion(questionIndex) {
     let q = questions[questionIndex];
     questionEl.textContent = (questionIndex + 1) + "." + q.question;
     opt1.textContent = q.option1;
     opt2.textContent = q.option2;
     opt3.textContent = q.option3;
     opt4.textContent = q.option4;
-};
+}
 
 /* Load next question function */
 
-function loadNextQuestion () {
+function loadNextQuestion() {
     let selectedOption = document.querySelector("input[type=radio]:checked");
     if (!selectedOption) {
         alert("Please select your answer!");
@@ -37,9 +37,7 @@ function loadNextQuestion () {
     let answer = selectedOption.value;
     if (questions[currentQuestion].answer === answer) {
         score += 1;
-    } else {
-        score -= 1;
-    }
+    } 
 
     scoreEl.innerHTML = `score: ${score}`;
 
@@ -51,9 +49,32 @@ function loadNextQuestion () {
     if (currentQuestion == totalQuestions) {
         container.style.display = "none";
         resultCont.style.display = "";
-        resultCont.textContent = "Your Score"   +  score   + "/" +  questions.length;
+        resultCont.textContent = "Your Score: " + score + "/" + questions.length;
+        restartQuizButton.style.display = "block"; // Show the restart button
+        resultCont.appendChild(restartQuizButton); // Append the restart button to the resultCont
         return;
     }
     loadQuestion(currentQuestion);
 }
+
+/* Event listeners */
+
+nextButton.addEventListener("click", loadNextQuestion);
+
+restartQuizButton.addEventListener("click", function restartQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    scoreEl.innerHTML = "Score: 0";
+    container.style.display = "block";
+    resultCont.style.display = "none";
+    restartQuizButton.style.display = "none"; // Hide the restart button
+    nextButton.textContent = "Next Question";
+    loadQuestion(currentQuestion);
+});
+
+/* Initial load */
 loadQuestion(currentQuestion);
+
+
+
+
